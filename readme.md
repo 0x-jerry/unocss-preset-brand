ed517f78fd1ca6115603ce35a141aacd02102658
0x-jerry <x.jerry.wang@gmail.com>
2024-03-29 16:40:24 +0800

    feat: generate brand css variables

--- readme.md
+++ readme.md
@@ -1,7 +1,45 @@
-# TS SDK Template
+# Uno Preset Brand
 
-## TODO
+## Install
 
-- [ ] run `npm init --yes`
-- [ ] change package metadata
-- [ ] rewrite this docs.
+```sh
+npm i @0x-jerry/unocss-preset-brand -D
+```
+
+```ts
+import { defineConfig, presetUno } from 'unocss'
+import { presetBrand } from '@0x-jerry/unocss-preset-brand'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetBrand({
+      brand: {
+        primary: 'teal',
+        gray: 'gray'
+      }
+    }),
+  ],
+})
+
+```
+
+## Usage 
+
+```html
+<div class="text-b-primary">
+  Primary Color
+</div>
+
+<div class="border-1 border-solid border-b-primary">
+  Use primary color with border
+</div>
+
+<div class="text-b-gray">
+  Gray Color
+</div>
+```
+
+### Change brand color at runtime
+
+TODO
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,124 @@
-export const hello = 'world'
+import { mapValues, pickBy, capitalize, kebabCase, mapKeys } from 'lodash-es'
+import { type Preflight, definePreset, entriesToCss } from 'unocss'
+import type { Theme } from 'unocss/preset-mini'
+
+export interface PresetBrandOption {
+  /**
+   * @default 'b'
+   */
+  prefix?: string
+
+  /**
+   * @default { primary: 'teal', gray: 'gray' }
+   */
+  brand?: Record<string, string>
+}
+
+const colorGradients = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950,
+]
+
+const defaultBrandColors = {
+  primary: 'teal',
+  gray: 'gray',
+}
+
+export const presetBrand = definePreset<PresetBrandOption>((options: PresetBrandOption = {}) => {
+  const varPrefix = 'brand'
+
+  const { brand: brandColors = defaultBrandColors, prefix = 'b' } = options
+
+  const colorName = (s: string) => prefix + capitalize(s)
+
+  const colors = mapKeys(
+    mapValues(brandColors, (_, key) => generateRefColors(key, varPrefix)),
+    (_, key) => colorName(key),
+  )
+
+  const preflights: Preflight<Theme>[] = [
+    {
+      layer: 'preflights',
+      getCSS(ctx) {
+        let _colors = pickBy(
+          ctx.theme.colors,
+          (v, key) => typeof v === 'object' && !Object.keys(colors).includes(key),
+        )
+
+        const preflight = generateColorVariables(_colors as any)
+
+        const brandVariables = generateBrandVariables(brandColors, varPrefix)
+
+        const _themeColorVariables = Object.assign({}, preflight, brandVariables)
+
+        const css = entriesToCss(Object.entries(_themeColorVariables))
+        return `:root {${css}}`
+      },
+    },
+  ]
+
+  return {
+    name: 'preset-brand',
+    theme: {
+      colors: colors,
+    },
+    preflights,
+  }
+})
+
+function generateColorVariables(colors: Record<string, Record<string, string>>) {
+  const variables: Record<string, string> = {}
+
+  colors = mapValues(colors, (colorObject) => mapValues(colorObject, convertHexToRgb))
+
+  for (const name in colors) {
+    for (const shade in colors[name]) {
+      if (!isNaN(parseInt(shade))) {
+        variables[`--${kebabCase(name)}-${shade}`] = colors[name][shade]
+      }
+    }
+  }
+
+  return variables
+}
+
+function hex2digit(hex: string) {
+  return parseInt(hex, 16)
+}
+
+function convertHexToRgb(color: string) {
+  if (color[0] !== '#') return color
+
+  const c = color.slice(1)
+  const r = hex2digit(c.slice(0, 2))
+  const g = hex2digit(c.slice(2, 4))
+  const b = hex2digit(c.slice(4, 6))
+
+  return `${r} ${g} ${b}`
+}
+
+function generateBrandVariables(brandColorMap: Record<string, string>, varPrefix: string) {
+  const variables: Record<string, string> = {}
+
+  for (const brandName in brandColorMap) {
+    const refColorName = brandColorMap[brandName]
+
+    for (const gradient of colorGradients) {
+      const varname = `--${varPrefix}-${brandName}-${gradient}`
+
+      variables[varname] = `var(--${refColorName}-${gradient})`
+    }
+  }
+
+  return variables
+}
+
+function generateRefColors(refColorName: string, varPrefix: string) {
+  const color: Record<string, string> = {}
+
+  for (const gradient of colorGradients) {
+    color[gradient] = `rgb(var(--${varPrefix}-${refColorName}-${gradient}))`
+  }
+
+  color.DEFAULT = color[400]
+  return color
+}
